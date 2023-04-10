b7b8dd61b879d81f9e3081336e578fbf9dfd874a
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-04-10 22:27:27 +0200

    feat: sql init script for docker

---
+++ docker/init.sql
@@ -0,0 +1,7 @@
+DROP DATABASE IF EXISTS find_n_rock;
+DROP DATABASE IF EXISTS dev_find_n_rock;
+DROP DATABASE IF EXISTS test_find_n_rock;
+
+CREATE DATABASE find_n_rock;
+CREATE DATABASE dev_find_n_rock;
+CREATE DATABASE test_find_n_rock;
