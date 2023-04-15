9666251330c79a570779cf31c130446671d56dc1
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-04-15 19:56:45 +0200

    feat: middleware for login

--- app/src/middleware/LoggedUser.middleware.ts
+++ app/src/middleware/LoggedUser.middleware.ts
@@ -4,14 +4,16 @@ import envConfig from '../config/DatabaseConfigurationConnection'
 
 export class LoggedUser {
 
-    isLoggedUser(req: Request) {
-        let token = req.cookies["auth-token"];
-        return token != null ? jwt.verify(token, envConfig.getSecretKey()) : false;
-    }
+    rejectIfNotLoggedIn(req: Request, res: Response, next: Function) {
+        function isLoggedUser(req: Request) {
+            let token = req.cookies["auth-token"];
+            return token != null ? jwt.verify(token, envConfig.getSecretKey()) : false;
+        }
 
-    rejectIfNotLoggedIn(req: Request, res: Response) {
-        if (!this.isLoggedUser(req)) {
-            return res.status(401).send();
+        if (!isLoggedUser(req)) {
+            return res.status(401).send("Unauthorized");
+        } else {
+            next();
         }
     }
 }
