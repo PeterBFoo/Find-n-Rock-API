ae2b1ab5c9a145b5446c803da3f34815cbde051c
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-04-24 10:42:00 +0200

    feat: constant responses

---
+++ app/src/static/Constants.ts
@@ -0,0 +1,17 @@
+
+export enum Constants {
+    POSTS_SUSCRIBE_NOT_ALLOWED = "You are not allowed to suscribe to any posts",
+    POSTS_SUSCRIBE_ALREADY_SUSCRIBED = "You are already suscribed to this post",
+    POSTS_UNSUSCRIBE_ALREADY_UNSUSCRIBED = "You are already not suscribed to this post",
+    POSTS_UNSUSCRIBE_OK = "You have been unsuscribed to this post",
+    POSTS_UNSUSCRIBE_ERROR = "An error ocurred, please try later",
+    POSTS_UNSUSCRIBE_NOT_ALLOWED = "You are not allowed to unsuscribe or suscribe to any posts",
+    POSTS_NOT_FOUND = "Post not found",
+    POSTS_CREATE_NOT_ALLOWED = 'Not allowed to create posts, only entrepreneours can create posts',
+    POSTS_EDIT_NOT_ALLOWED = "Only the post owner can update this post",
+    POSTS_DELETE_NOT_ALLOWED = "Only the post owner can delete this post",
+    GENRES_INVALID = "Some of the genres requested does not exist",
+    USER_NOT_FOUND = "User not found",
+    GENERAL_ERROR = "Something went wrong, please try later",
+    BAD_REQUEST = "Invalid data, please ensure that you are sending the requested data correctly"
+}
