77b8594448f95311e5040dfaa336165b9aa448ea
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-04-27 23:49:35 +0200

    refactor: response

--- app/src/controllers/RegisterController.ts
+++ app/src/controllers/RegisterController.ts
@@ -47,7 +47,7 @@ export class RegisterController {
             return !Object.getOwnPropertyNames(result).includes("error") ? res.status(200).send(user) : res.status(400).send(result);
         }
         catch (err: any) {
-            return res.status(500).send({ error: err.message });
+            return res.status(400).send({ error: err });
         }
     }
 
